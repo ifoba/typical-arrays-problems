b27915fbc1da0c1bf701504bc4c2e9fd336b2f01
Maxim Ryabov <foba1995@gmail.com>
2020-02-28 00:59:19 +0400

    init:start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+        return Math.min.apply(null, array);}
+      return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+      return Math.max.apply(null, array);}
+    return 0;
 }
 
 exports.avg = function avg (array) {
+    let sum=0;
+    if(typeof array != "undefined" && array != null && array.length != null && array.length > 0){
+        for(let i=0; i<array.length; i++){
+            sum+=array[i];
+        }
+        return sum/array.length;}
   return 0;
 }
